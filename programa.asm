3b9672100f5440c5693ad0d8e6a9c54af1b67418
Pedro Saman <pedrosaman96@gmail.com>
2018-10-11 19:58:53 -0300

    mudei para testar melhor o pre processamento

--- programa.asm
+++ programa.asm
@@ -1,6 +1,20 @@
-SUB SUB 
-MULT ADD SUB
-COPY
-SUB            ADD
-ADD
-COPY
+Pinda: EQU 0
+TAM: EQU 1
+BMDS: EQU 2
+SECTION               TEXT
+ROT: INPUT N1;comentario
+COPY N1, N4 ;comentario qualquer
+ROT2:
+COPY N2, N3
+COPY N3, N3 + 1
+IF TAM
+OUTPUT N3 + 1
+STOP
+IF Pinda
+;comentario qualuer
+SECTIO DATA
+N2:  CONST -5
+           SECTION BSS
+N1:  SPACE Pinda
+N3:       SPACE      BMDS
+OUTPUT TAM BMDS TAM TAM TAM TAM         Pinda
