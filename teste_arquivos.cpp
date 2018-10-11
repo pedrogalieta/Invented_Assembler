b7228f2d75843f507754be07b6df84bb157a9c36
pedrogalieta <pedrogalieta@hotmail.com>
2018-10-10 21:21:46 -0300

    Reconhece instruções e tem elas salvas em map de pair com pair

--- teste_arquivos.cpp
+++ teste_arquivos.cpp
@@ -1,123 +1,98 @@
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <fstream>
 #include <map>
-#include <ctype.h>
 
 using namespace std;
 
-void insere_diretivas_no_mapa(map<string,int> &tabela_de_diretivas){ //Função que insere as chaves no map de instruções
+int main () {
 
-     tabela_de_diretivas.insert(make_pair("SECTION",0));
-     tabela_de_diretivas.insert(make_pair("SPACE",1));
-     tabela_de_diretivas.insert(make_pair("CONST",2));
-     tabela_de_diretivas.insert(make_pair("PUBLIC",3));
-     tabela_de_diretivas.insert(make_pair("EQU",4));
-     tabela_de_diretivas.insert(make_pair("IF",5));
-     tabela_de_diretivas.insert(make_pair("EXTERN",6));
-     tabela_de_diretivas.insert(make_pair("BEGIN",7));
-     tabela_de_diretivas.insert(make_pair("END",8));
+  string linha;
+  string palavra;
+  int i;
+  int length_linha;
+  ifstream arq_fonte;
+  std::map<string, pair<int,int> > i_map; //mapa de instruções
 
-}
 
-void insere_instrucoes_no_mapa(map<string,int> &tabela_de_instrucoes){ //Função que insere as chaves no map de instruções
+  i_map.insert(make_pair("ADD", make_pair(1, 3)));
+  i_map.insert(make_pair("SUB", make_pair(2, 3)));
+  i_map.insert(make_pair("MULT", make_pair(3, 3)));
+  i_map.insert(make_pair("DIV", make_pair(4, 3)));
+  i_map.insert(make_pair("JMP", make_pair(5, 3)));
+  i_map.insert(make_pair("JMPN", make_pair(6, 3)));
+  i_map.insert(make_pair("JMPP", make_pair(7, 3)));
+  i_map.insert(make_pair("JMPZ", make_pair(8, 3)));
+  i_map.insert(make_pair("COPY", make_pair(9, 3)));
+  i_map.insert(make_pair("LOAD", make_pair(10, 3)));
+  i_map.insert(make_pair("STORE", make_pair(11, 3)));
+  i_map.insert(make_pair("INPUT", make_pair(12, 3)));
+  i_map.insert(make_pair("OUTPUT", make_pair(13, 3)));
+  i_map.insert(make_pair("STOP", make_pair(14, 3)));
 
-     tabela_de_instrucoes.insert(make_pair("ADD",1));
-     tabela_de_instrucoes.insert(make_pair("SUB",2));
-     tabela_de_instrucoes.insert(make_pair("MULT",3));
-     tabela_de_instrucoes.insert(make_pair("DIV",4));
-     tabela_de_instrucoes.insert(make_pair("JMP",5));
-     tabela_de_instrucoes.insert(make_pair("JMPN",6));
-     tabela_de_instrucoes.insert(make_pair("JMPP",7));
-     tabela_de_instrucoes.insert(make_pair("JMPZ",8));
-     tabela_de_instrucoes.insert(make_pair("COPY",9));
-     tabela_de_instrucoes.insert(make_pair("LOAD",10));
-     tabela_de_instrucoes.insert(make_pair("STORE",11));
-     tabela_de_instrucoes.insert(make_pair("INPUT",12));
-     tabela_de_instrucoes.insert(make_pair("OUTPUT",13));
-     tabela_de_instrucoes.insert(make_pair("STOP",14));
+  arq_fonte.open("programa.asm");
 
-}
+  if(!arq_fonte.is_open()){
 
-int checa_abertura_dos_arquivos(ifstream &programa, fstream &simbolos){ //Checa se os arquivo foram abertos corretamente
+    cout << "Impossível abrir arquivo solicitado!";
+    exit(1);
+  }
 
-     if (!(programa.is_open() && programa.good())){
-       cout << "Arquivo Fonte não foi achado \nFinalizando programa";
-       return -1;
-     }
+  while(getline(arq_fonte, linha)){
 
-    if (!simbolos.is_open()){
-      cout << "Tabela de simbolos não aberta corretamente \nFinalizando programa";
-      return -1;
-    }
-    return 1;
-}
+    length_linha = linha.length();
 
-int main(){
+    //varre a linha, separando cada palavra
+    for(i=0; i<length_linha; i++){
 
-     int i,j,k,tamanho_da_linha,x;
-     string aux1;
-     string aux2;
-     string linha_codigo_fonte;
-     string palavra_codigo_fonte;
-     string palavra_da_tabela;
-     string inicio_comentario = ";";
-     map<string,int> tabela_de_instrucoes;
-     map<string,int> tabela_de_diretivas;
+      palavra += linha[i];
 
-     ifstream arquivo_fonte;		        //Cria os objetos para o arquivo fonte
-     fstream tabela_de_simbolos;           //Cria o objeto para a tabela de simbolos para leitura e escrita
-     arquivo_fonte.open("programa.asm");   //Abre os arquivos para o codigo fonte e a tabela de simbolos
-     tabela_de_simbolos.open("tabela_de_simbolos.txt", fstream::app | fstream::in);
+      //fim de palavra
+      if(linha[i] == ' '){
 
-     if (!(checa_abertura_dos_arquivos(arquivo_fonte,tabela_de_simbolos))){ //Mata o programa caso não consiga abrir os arquivos
-            return -1;
-     }
+        if(palavra.length() > 1){
+          
+          //remove espaço
+          palavra.erase(palavra.end()-1);
 
-    insere_instrucoes_no_mapa(tabela_de_instrucoes); //Insere as instruções no mapa
-    insere_diretivas_no_mapa(tabela_de_diretivas); //Insere as diretivas no mapa
+          if (i_map.find(palavra) != i_map.end()){
 
-    // Fazer uma Função que faz isso. Esse while pega todas as linhas do arquivo asm e verifica se cada palavra é ou não uma instrução e devolve o opcode
-    while(!arquivo_fonte.fail()){ //Itera sobre o arquivo assembly
-          getline(arquivo_fonte,linha_codigo_fonte); //Pega linha por linha do arquivo assembly
-          i=0;
-          k=0;
-          tamanho_da_linha = linha_codigo_fonte.length();
-
-          //Esse while itera pela linha lida pelo getline
-          while(i<tamanho_da_linha){
-               for(j=0;linha_codigo_fonte[i]!=' ' ;j++,i++){
-                    palavra_codigo_fonte += linha_codigo_fonte[i];
-                    if(j+k >= tamanho_da_linha){
-                         palavra_codigo_fonte.erase(j,palavra_codigo_fonte.length());
-                         break;
-                    }
-               }
-
-               // Esse if interrompe a leitura do resto da linha se lermos um ;
-               if (palavra_codigo_fonte == inicio_comentario){
-                    palavra_codigo_fonte.erase(0,palavra_codigo_fonte.length());
-                    break;
-               }
-
-               // Esse if else escreve na tela se a palavra lida da linha é ou não uma instrução
-               if(tabela_de_instrucoes.find(palavra_codigo_fonte) != tabela_de_instrucoes.end()){
-                         cout << "opcode de " << palavra_codigo_fonte << " é: " << tabela_de_instrucoes.find(palavra_codigo_fonte)->second << endl;
-               }else if(palavra_codigo_fonte.length() != 0){ //Não precisa checar caso a string seja NULL
-                     cout<< "Não achamos:" << palavra_codigo_fonte << "no map " << endl;
-               }
-
-               palavra_codigo_fonte.erase(0,palavra_codigo_fonte.length()); //Apaga a string palavra_codigo_fonte
-
-               //Variáveis de controle para esse código iterar corretamente
-               k+=j;
-               k++;
-               i++;
+            cout << palavra << ' ' << i_map.find(palavra)->second.first << endl;
           }
+          else{
 
-     }
+            cout << palavra << ' ' << "não existente!" << endl;
+          }
+        }
 
-    arquivo_fonte.close(); // fecha o arquivo após usa-lo
+        palavra.clear();
+      }
+
+      //fim de linha (e de uma palavra)
+      else if(i == (length_linha - 1)){
+
+        if (i_map.find(palavra) != i_map.end()){
+
+          cout << palavra << ' ' << i_map.find(palavra)->second.first << endl;
+        }
+        else{
+
+          cout << palavra << ' ' << "não existente!" << endl;
+        }
+
+        palavra.clear();
+      }
+    }
+  }
+
+  arq_fonte.close();
+
+  /*TESTE DO CONTEÚDO DE I_MAP
+  cout << "Conteúdo do i_map:" << endl;
+  for (map<string, pair<int, int> >::iterator it = i_map.begin(); it != i_map.end(); it++){
+
+    cout << it->first << ' ' << it->second.first << ' ' << it->second.second <<  endl;
+  }*/
 
   return 0;
 }
