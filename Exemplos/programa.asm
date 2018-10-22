a94285a970f1024c56f40f3d2ad23bde201b04a2
Pedro Saman <pedrosaman96@gmail.com>
2018-10-22 13:05:18 -0300

    Criei uma pasta para os exemplos

--- Exemplos/programa.asm
+++ Exemplos/programa.asm
@@ -1,5 +1,8 @@
-Pinda: EQU 0
-TAM: EQU 1
+Pinda:  EQU   1
+Pinda:  EQU          1
+TAM2: EQU 1
+
+TAM: EQU 0
 BMDS: EQU 2
 SECTION               TEXT
 ROT: INPUT N1;comentario
@@ -7,14 +10,13 @@ COPY N1, N4 ;comentario qualquer
 ROT2:
 COPY N2, N3
 COPY N3, N3 + 1
-IF TAM
 OUTPUT N3 + 1
+IF TAM
 STOP
-IF Pinda
 ;comentario qualuer
 SECTIO DATA
 N2:  CONST -5
            SECTION BSS
 N1:  SPACE Pinda
 N3:       SPACE      BMDS
-OUTPUT TAM BMDS TAM TAM TAM TAM         Pinda
+N4: SPACE
